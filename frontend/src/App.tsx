fc7659f9af9764463c3fc5cbf1a6f9445a0db534
nystrand1 <filip.nystrand@gmail.com>
2021-02-05 15:49:16 +0100

    Added rechart package

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Api from './services/Api';
+import React, { useEffect, useState } from "react";
+import {
+  ScatterChart,
+  Scatter,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+import logo from "./logo.svg";
+import "./App.css";
+import Api from "./services/Api";
+import ScatterGraph from "./components/graphs/ScatterGraph";
+import LineGraph from "./components/graphs/LineGraph";
 
-const App = () =>  {
+const App = () => {
   const [loading, setLoading] = useState(true);
   const [text, setText] = useState("Loading...");
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("victoria beckham");
   useEffect(() => {
-    Api.post("count", {
-      query: "victoria"
+    Api.post("count-interval", {
+      query: query,
+      interval_mins: 60*24,
+      days_ago: 360,
     }).then((res) => {
-      setText(res);
+      console.log(res);
+      console.log(res[0]);
+      setData(res);
       setLoading(false);
-    })
+    });
   }, []);
   return (
     <div className="App">
       <header className="App-header">
-        {text}
+        {loading ? (
+          text
+        ) : (
+          <LineGraph data={data} xLabel={"Time"} yLabel={"Count"} zLabel={"Query"} title={query}/>
+        )}
       </header>
     </div>
   );
-}
+};
 
 export default App;
