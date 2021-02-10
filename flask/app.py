0227182866892225ce569a37dd03015b6875f464
nystrand1 <filip.nystrand@gmail.com>
2021-02-10 15:12:51 +0100

    Changed query to postgres. Includes zero points

--- flask/app.py
+++ flask/app.py
@@ -56,26 +56,34 @@ def count_query_interval():
     interval_mins = data['interval_mins']
     days_ago = data['days_ago']
     res = db.session.execute("""
-        SELECT :query, count(search_records.query) as count, 
-        UNIX_TIMESTAMP(created_at) DIV (60 * :mins) as timekey
-        FROM search_records
-        WHERE
-        search_records.query LIKE :query
+        SELECT query, to_char(series.minute, 'YYYY-MM-DD HH24:MI:SS') as interval, coalesce(count.amount,0) as count from 
+        (
+        SELECT query, count(query) amount,
+        TIMESTAMP WITH TIME ZONE 'epoch' +
+        INTERVAL '1 second' * round(extract('epoch' from created_at) / (:mins * 60)) * (:mins * 60) as interval
+        FROM search_records.search_record  
+        WHERE query LIKE :query
         AND
-        search_records.created_at >= now() - interval :days day
-        GROUP BY
-        timekey, :query
+        created_at >= '2021-02-02'::date - :days
+        GROUP BY round(extract('epoch' from created_at) / (:mins * 60)), query
+        ) count
+
+        RIGHT JOIN 
+        (
+        SELECT generate_series(max(date_trunc('minute',created_at::date - :days)),
+        max(date_trunc('minute', '2021-02-02'::date)),':mins m') as minute from search_records.search_record
+        ) series
+        on series.minute = count.interval
         ORDER BY
-        timekey DESC
+        interval DESC
         """, {
             'query': query,
             'mins': interval_mins,
-            'days': days_ago 
+            'days': days_ago ,
         })
     res_arr = []
     for r in res:
         r = dict(r.items())
-        r['time'] = datetime.datetime.utcfromtimestamp(r['timekey']*60*interval_mins).strftime('%Y-%m-%d %H:%M:%S')
         res_arr.append(r)
     res_arr.reverse()
     return Response(json.dumps(res_arr), status=200, content_type="application/json")
