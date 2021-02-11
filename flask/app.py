a53a83f8250a42ca8c471fd33a5c9c10b3932f5b
nystrand1 <filip.nystrand@gmail.com>
2021-02-11 18:00:41 +0100

    Cleanup

--- flask/app.py
+++ flask/app.py
@@ -9,7 +9,7 @@ from flask_sqlalchemy import Model
 from flask_cors import CORS, cross_origin
 
 from .models.search_record import SearchRecord
-from .functions.count_interval import count_interval
+from .functions.count_interval import count_interval_grouped, count_interval_individual
 from .functions.regression.linear import get_linear_model
 
 
@@ -18,6 +18,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = config('SQLALCHEMY_DATABASE_URI')
 cache = redis.Redis(host='redis', port=6379)
 db = sa(app)
 CORS(app, resources={r"/*": {"origins": "*"}})
+
 
 def get_hit_count():
     retries = 5
@@ -30,41 +31,64 @@ def get_hit_count():
             retries -= 1
             time.sleep(0.5)
 
+
 @app.route('/')
 @cross_origin()
 def hello():
     res = db.session.query(SearchRecord).filter_by(query='nike').count()
     return 'Hello World! I have seen "nike" {} times.\n'.format(res)
 
+
 @app.route('/count', methods=['POST'])
 @cross_origin()
 def count_query():
     data = request.json
     query = data['query']
-    res = db.session.query(SearchRecord).filter(SearchRecord.query.contains(query)).count()
+    res = db.session.query(SearchRecord).filter(
+        SearchRecord.query.contains(query)).count()
     return 'Hello World! I have seen messages like "{}" {} times.\n'.format(query, res)
 
-@app.route('/count-interval', methods=['POST'])
+
+@app.route('/count-interval-grouped', methods=['POST'])
 @cross_origin()
-def count_query_interval():
+def count_query_interval_grouped():
     data = request.json
     if 'query' not in data:
         return Response("Missing parameter 'query'", status=400)
     if 'interval_mins' not in data:
         return Response("Missing parameter 'interval_mins'", status=400)
-    if 'days_ago' not in data:
-        return Response("Missing parameter 'days_ago'", status=400)
 
     query = data['query']
     interval_mins = data['interval_mins']
-    days_ago = data['days_ago']
-    res = count_interval(db, query, interval_mins, days_ago)
-    return Response(json.dumps(res), status=200, content_type="application/json")
-    
+    start_date = data['start_date']
+    end_date = data['end_date']
+    res = count_interval_grouped(
+        db, query, interval_mins, start_date, end_date)
+    return Response(json.dumps(res, indent=4, sort_keys=True, default=str), status=200, content_type="application/json")
+
+
+@app.route('/count-interval-individual', methods=['POST'])
+@cross_origin()
+def count_query_interval_individual():
+    data = request.json
+    if 'query' not in data:
+        return Response("Missing parameter 'query'", status=400)
+    if 'interval_mins' not in data:
+        return Response("Missing parameter 'interval_mins'", status=400)
+
+    query = data['query']
+    interval_mins = data['interval_mins']
+    start_date = data['start_date']
+    end_date = data['end_date']
+    res = count_interval_individual(
+        db, query, interval_mins, start_date, end_date)
+    return Response(json.dumps(res, indent=4, sort_keys=True, default=str), status=200, content_type="application/json")
+
+
 @app.route('/linear-regression', methods=['POST'])
 @cross_origin()
 def linear_regression():
-    res = count_interval(db)
+    res = count_interval_grouped(db)
     linear_model = get_linear_model(res)
 
     return Response(json.dumps({
