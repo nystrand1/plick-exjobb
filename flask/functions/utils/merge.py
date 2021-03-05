80e742b9d7538447dd049abd577e257fc8d4bf83
nystrand1 <filip.nystrand@gmail.com>
2021-03-05 16:32:38 +0100

    more logging in merge

--- flask/functions/utils/merge.py
+++ flask/functions/utils/merge.py
@@ -8,9 +8,9 @@ def merge_datasets(dataset_1, dataset_2, key_name = "degree 1"):
 
 def merge_partial_dataset(dataset_1, dataset_2, key_name = "degree 1"):
     start_index = len(dataset_1) - len(dataset_2)
-    logging.debug(start_index)
-    logging.debug(len(dataset_2))
-    logging.debug(len(dataset_1))
+    logging.debug("START INDEX: {}".format(start_index))
+    logging.debug("LENGTH DATASET 2: {}".format(len(dataset_2)))
+    logging.debug("LENGTH DATASET 1: {}".format(len(dataset_1)))
     for (i,data) in enumerate(dataset_1[start_index:]):
         data['trends'][key_name] = dataset_2[i]
         start_index+=1
